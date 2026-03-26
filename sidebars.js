068543025fd8d5d7d360f0eab6fb29407ddb46da
zhuyedylan <zhuyedylan@gmail.com>
2026-03-26 18:14:07 +0800

    Update: 侧边栏菜单改为中文

--- sidebars.js
+++ sidebars.js
@@ -1,16 +1,16 @@
 module.exports = {
   docs: {
     ' Getting Started': ['intro', 'register', 'profile'],
-    'Core Features': [
+    '核心功能': [
       'main-features',
       'membership',
     ],
-    'Health Habits': ['habits'],
-    'Exercise': ['exercise'],
-    'Health Evaluation': ['evaluation'],
-    'Manual Check-in & Tests': ['test'],
-    'Challenges': ['challenge'],
-    'Clubs': ['club'],
-    'AI Reports': ['ai-report'],
+    '健康习惯': ['habits'],
+    '锻炼功能': ['exercise'],
+    '健康评估': ['evaluation'],
+    '手动打卡和测试': ['test'],
+    '挑战功能': ['challenge'],
+    '俱乐部功能': ['club'],
+    'AI报告与分析': ['ai-report'],
   },
 };
