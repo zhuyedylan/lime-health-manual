e5ad2d0f04ed43804b2940fc1fde468a1e88831f
zhuyedylan <zhuyedylan@gmail.com>
2026-03-26 18:08:12 +0800

    Update: 修复文档链接+添加说明

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require("/Users/dylanmba/Documents/NingHealth_App_Manual/node_modules/infima/dist/css/default/default.css"),
+  require("/Users/dylanmba/Documents/NingHealth_App_Manual/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
+  require("/Users/dylanmba/Documents/NingHealth_App_Manual/node_modules/@docusaurus/theme-classic/lib/nprogress"),
+  require("/Users/dylanmba/Documents/NingHealth_App_Manual/src/css/custom.css"),
+];
